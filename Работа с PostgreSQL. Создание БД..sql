a97e74cb5d63f653234f3ff93579754a5ff06ec2
Fyodor Sh <82056292+fshakrun@users.noreply.github.com>
2021-12-11 18:48:11 +0300

    Add files via upload

---
+++ Работа с PostgreSQL. Создание БД..sql
@@ -0,0 +1,18 @@
+create table if not exists Artist_list(
+	Id serial primary key,
+	Artist_name varchar(40) not null
+);
+
+create table if not exists Album(
+	Id serial primary key,
+	Album_title varchar(40) not null unique,
+        Release_date integer(4,0) not null,
+	Artist_Id integer references Artist_list(Id) not null
+);
+
+create table if not exists Tracks(
+	Id serial primary key,
+    Track_title varchar(100) not null unique,
+    Track_duration integer(4,0) not null,
+	Album_Id integer references Album(Id)
+);
